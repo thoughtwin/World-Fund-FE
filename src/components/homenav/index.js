62cac19766b93189f4b147f9b3434b9f91b63ace
Kratika <iamdeveloper.me@gmail.com>
2021-05-14 18:29:35 +0530

    create build

--- src/components/homenav/index.js
+++ src/components/homenav/index.js
@@ -5,7 +5,7 @@ const HomeNav = () => {
   const history = useHistory();
   return (
     <>
-      {/* <nav className="navbar navbar-inverse">
+      <nav className="navbar navbar-inverse">
         <div className="container-fluid">
           <div className="navbar-header">
             <Link className="navbar-brand" to="/home"><img src={Logo} /></Link>
@@ -22,25 +22,6 @@ const HomeNav = () => {
             <li><button className="get_startedbtn" onClick={() => history.push('/signup')}>Get started</button></li>
           </ul>
         </div>
-      </nav> */}
-      <nav class="navbar navbar-expand-md navbar-inverse fixed_header">
-      <Link className="navbar-brand" to="/home"><img src={Logo} /></Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarHeader">
-          <ul class="navbar-nav homemenulist">
-            <li className="active"><Link to="/about">About Us</Link></li>
-            <li><Link to="/faq">FAQ</Link></li>
-            <li><Link to="/rules">Rules</Link></li>
-            <li><Link to="/news">News</Link></li>
-            <li><Link to="/contact">Contact</Link></li>  
-          </ul>
-          <ul className="nav navbar-nav navbar-right right_sidenav">
-            <li className="logintext"><Link to="/login"> <i className="fa fa-lock"></i>Log in</Link></li>
-            <li><button className="get_startedbtn">Get started</button></li>
-          </ul>
-        </div>  
       </nav>
     </>
   )
