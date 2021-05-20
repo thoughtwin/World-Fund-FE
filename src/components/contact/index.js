382a2ffbd715cec9237b7118840054ca53cd383b
pankaj <pankajagrawaltw@gmail.com>
2021-05-20 18:38:56 +0530

    working on rules page with reponsive

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,11 +1,83 @@
 import React from 'react';
 import HomeNav from '../homenav';
+import chat_small from '../../assets/images/chat_small.png';
+import Chat_big from '../../assets/images/Chat_big.png';
+import Logo from '../../assets/images/Logo.png';
+import Base from '../../assets/images/Base.png';
+import Footer_wave from '../../assets/images/Footer_wave.png';
+import './contact.css';
 const Contact = () => {
     return(
+        <div className="home_maincontainer rules_page">
+        <div className="ourpolicies_container">
         <div className="container">
-            <HomeNav/>
-            <h1>Contact Page</h1>
+            <HomeNav />
         </div>
+        </div>
+
+        <div className="privacy_policycontainer rule_container" style={{backgroundImage:`url(${Base})`}}>
+        <div className="container">
+            <span>Home  /  FAQ</span>
+            <h2>Contact</h2>
+        </div>
+        </div>
+
+ 
+
+
+
+
+
+
+
+
+
+
+    <div className="footer_container" style={{backgroundImage:`url(${Footer_wave})`}}>
+      <div className="row container container_row footer">
+        <div className="col-md-3 margin_bottom">
+          <img src={Logo} />
+        </div>
+        <div className="col-md-2 margin_bottom">
+          <ul>
+            <li>Main</li>
+            <li>Investments</li>
+            <li>Partner program</li>
+            <li>News</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div className="col-md-2 margin_bottom">
+          <ul>
+            <li>Home</li>
+            <li>Get Started</li>
+            <li>Institutions</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="col-md-3 margin_bottom">
+          <ul>
+            <li>Privacy policy</li>
+            <li>Risk disclosure</li>
+            <li>AML policy</li>
+          </ul>
+        </div>
+        <div className="col-md-2 margin_bottom">
+          <ul className="footer_email">
+            <li className="footer_emailicon"><i className="fa fa-envelope"></i>info@cia.com</li>
+            <li><span className="footericon"><i className="fa fa-facebook-f"></i></span><span className="footericon_paper"><i className="fa fa-paper-plane"></i></span></li>
+          </ul>
+        </div>
+      </div>
+      <div className="container copyright"><i className="fas fa-paper-plane"></i>
+        <p>Copyright © 2020 World Funds All rights reserved.</p>
+      </div>
+      <div className="chat_btns">
+        <p className="chat_withus">Chat with us<img src={chat_small} /></p>
+        <p><img src={Chat_big} /></p>
+      </div>
+    </div>
+  </div>
     )
 }
 
--- src/components/rules/index.js
+++ src/components/rules/index.js
@@ -1,11 +1,161 @@
 import React from 'react';
 import HomeNav from '../homenav';
+import chat_small from '../../assets/images/chat_small.png';
+import Chat_big from '../../assets/images/Chat_big.png';
+import Logo from '../../assets/images/Logo.png';
+import Base from '../../assets/images/Base.png';
+import Footer_wave from '../../assets/images/Footer_wave.png';
+import './rules.css';
 const Rules = () => {
     return(
-        <div className="container">
-            <HomeNav/>
-            <h1>Rules Page</h1>
+        <div className="home_maincontainer rules_page">
+            <div className="ourpolicies_container">
+            <div className="container">
+                <HomeNav />
+            </div>
+            </div>
+  
+            <div className="privacy_policycontainer rule_container" style={{backgroundImage:`url(${Base})`}}>
+                <div className="container">
+                    <span>Home  /  rules</span>
+                    <h2>Our Rules</h2>
+                </div>
+            </div>
+            <div className="container rules_data">
+               <div className="row">
+                   <div className="col-md-12">
+                        <p className="pb-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh tortor, eget convallis pretium 
+                        ornare mi turpis. A proin viverra aenean egestas dolor felis elementum fermentum. Molestie 
+                        fermentum tempus aliquam ipsum dolor dui enim adipiscing. Suspendisse fringilla vitae nunc 
+                        id quis et habitant augue. Luctus condimentum nullam eu quis nisl est facilisis. Blandit
+                         nulla nisl volutpat magna. Ut maecenas id sed quis morbi dignissim eu non. Ultricies tortor, non, elit eu. Nec dignissim pretium
+                          pellentesque  laoreet tellus pharetra, diam. Leo aliquet diam ultrices placerat vestibulum, enim ac et. 
+                        Lacinia quam facilisis amet vel enim.  Pretium porttitor adipiscing integer sed sit diam habitant fermentum. Adipiscing diam amet
+                         amet natoque lacus. Ut quis tincidunt in.
+                        </p>
+                        <p>
+                        Lorem netus porttitor ut nulla laoreet ultrices. Gravida tellus sapien, mi amet. Lacus, duis sed senectus mauris dignissim. 
+                        Dignissim eu sit pellentesque odio. Congue erat lobortis mauris lorem porta mi vel quis. Amet ornare dolor nisi enim nunc, massa nibh mattis. 
+                        Cursus non amet vitae feugiat. Arcu egestas massa sed.
+                        </p>
+                   </div>
+                   <div className="col-md-12">
+                    <h2>I. Rule first</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat feugiat duis eget arcu dis enim. 
+                        A in blandit turpis tellus eu eu. Tempor laoreet non at suspendisse nulla ligula. Posuere velit eget blandit arcu volutpat urna. 
+                        Eu lectus vel neque nisi arcu eu, eget. Lobortis facilisi facilisi nec vitae. Pharetra, est elit vitae vitae. Magna ipsum id orci massa et vitae. 
+                        Interdum phasellus viverra a dis viverra orci orci. A diam etiam venenatis habitasse duis sit urna. Tempor a viverra adipiscing vitae nisi, purus tempus. 
+                        Tempus lacus faucibus viverra quis lectus metus. Scelerisque egestas ultrices aliquam amet consectetur pellentesque eget. Diam cras lacus id venenatis, tincidunt.
+                        </p>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat feugiat duis eget arcu dis enim. 
+                        A in blandit turpis tellus eu eu. Tempor laoreet non at suspendisse nulla ligula. Posuere velit eget blandit arcu volutpat urna. 
+                        Eu lectus vel neque nisi arcu eu, eget. Lobortis facilisi facilisi nec vitae. Pharetra, est elit vitae vitae. Magna ipsum id orci massa et vitae. 
+                        </p>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat feugiat duis eget arcu dis enim. 
+                        A in blandit turpis tellus eu eu. Tempor laoreet non at suspendisse nulla ligula. Posuere velit eget blandit arcu volutpat urna. 
+                        Eu lectus vel neque nisi arcu eu, eget. Lobortis facilisi facilisi nec vitae. Pharetra, est elit vitae vitae. Magna ipsum id orci massa et vitae. 
+                        </p>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat feugiat duis eget arcu dis enim. 
+                        A in blandit turpis tellus eu eu. Tempor laoreet non at suspendisse nulla ligula. Posuere velit eget blandit arcu volutpat urna. 
+                        Eu lectus vel neque nisi arcu eu, eget. Lobortis facilisi facilisi nec vitae. Pharetra, est elit vitae vitae. Magna ipsum id orci massa et vitae. 
+                        Interdum phasellus viverra a dis viverra orci orci. A diam etiam venenatis habitasse duis sit urna. 
+                        </p>   
+                    <h2> II. Rule second </h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit pellentesque enim nulla
+                         sagittis. Congue tristique ligula dignissim at. Posuere feugiat adipiscing ultricies 
+                         elementum. Amet pharetra in orci dictumst velit egestas dui. 
+                        Vel et proin tincidunt sed eu tempus, platea sapien. Sed at id in fermentum.
+                        </p>
+                        <p>
+                        Gravida tristique rhoncus, sed nunc sed lorem enim rhoncus. Ut ullamcorper accumsan
+                         ullamcorper eget aliquam. Ornare arcu consequat aliquam accumsan in. Lacus at et, nulla 
+                         feugiat eget sit. Non est vulputate amet, arcu in quis sed risus. Ullamcorper fringilla ut
+                          quis malesuada. Tellus id orci justo, libero, volutpat convallis sit dolor. Enim mauris 
+                          magnis proin tempor, semper eu nunc sem. Sapien non eget proin tincidunt sit.
+                         Faucibus nulla tellus rutrum sociis duis interdum.
+                        </p>
+                      
+                    <h2>III. Rule third</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget vitae, hac eu.
+                         Ante magna scelerisque quis amet, sem luctus. Id purus venenatis, a enim aliquam elit. 
+                         Sagittis quis consectetur felis nisl fringilla malesuada lorem. Vulputate libero pellentesque elementum,
+                         lacinia nulla iaculis commodo. Lacinia diam tristique.
+                        </p>           
+                      <h2>IV. Rule fourth</h2>        
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat feugiat duis eget arcu dis enim. 
+                        A in blandit turpis tellus eu eu. Tempor laoreet non at suspendisse nulla ligula. Posuere velit eget blandit arcu volutpat urna. 
+                        </p>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat feugiat duis eget arcu dis enim. 
+                        </p>
+                   </div>
+               </div>
+            </div>
+  
+     
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+        <div className="footer_container" style={{backgroundImage:`url(${Footer_wave})`}}>
+          <div className="row container container_row footer">
+            <div className="col-md-3 margin_bottom">
+              <img src={Logo} />
+            </div>
+            <div className="col-md-2 margin_bottom">
+              <ul>
+                <li>Main</li>
+                <li>Investments</li>
+                <li>Partner program</li>
+                <li>News</li>
+                <li>FAQ</li>
+              </ul>
+            </div>
+            <div className="col-md-2 margin_bottom">
+              <ul>
+                <li>Home</li>
+                <li>Get Started</li>
+                <li>Institutions</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="col-md-3 margin_bottom">
+              <ul>
+                <li>Privacy policy</li>
+                <li>Risk disclosure</li>
+                <li>AML policy</li>
+              </ul>
+            </div>
+            <div className="col-md-2 margin_bottom">
+              <ul className="footer_email">
+                <li className="footer_emailicon"><i className="fa fa-envelope"></i>info@cia.com</li>
+                <li><span className="footericon"><i className="fa fa-facebook-f"></i></span><span className="footericon_paper"><i className="fa fa-paper-plane"></i></span></li>
+              </ul>
+            </div>
+          </div>
+          <div className="container copyright"><i className="fas fa-paper-plane"></i>
+            <p>Copyright © 2020 World Funds All rights reserved.</p>
+          </div>
+          <div className="chat_btns">
+            <p className="chat_withus">Chat with us<img src={chat_small} /></p>
+            <p><img src={Chat_big} /></p>
+          </div>
         </div>
+      </div>
     )
 }
 
